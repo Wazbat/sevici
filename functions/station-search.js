127b9289265d71924cb6c2813c7a5223dcf9841e
Warren <wazbat97@gmail.com>
2019-12-10 12:27:17 +0100

    :clown_face: Added missing Image import and fixed indentation

--- functions/station-search.js
+++ functions/station-search.js
@@ -1,4 +1,4 @@
-const { BasicCard, Button }  = require("actions-on-google");
+const { BasicCard, Button, Image }  = require("actions-on-google");
 const { buildStationString } = require("../utils");
 const { getDirection } = require("../utils");
 const geolib = require('geolib');
@@ -7,77 +7,77 @@ const seviciService = new Sevici(process.env.JCDECAUXAPIKEY);
 const buildUrl = require('build-url');
 module.exports = {
     async specificStationSearch (conv) {
-    const { location } = conv.device;
-    /*
-    {
-        coordinates: { latitude: 36.8775256, longitude: -5.4021203 },
-        formattedAddress: 'Cádiz, Algodonales, 11680',
-        zipCode: '11680',
-        city: 'Algodonales'
-      }
-     */
-    const user = conv.user;
-    /*
-    console.log('Previous event', conv.data.event);
-    conv.followup(conv.data.event, {
-        coordinates: location.coordinates,
-        ...conv.data.originalParams
-    });
+        const { location } = conv.device;
+        /*
+        {
+            coordinates: { latitude: 36.8775256, longitude: -5.4021203 },
+            formattedAddress: 'Cádiz, Algodonales, 11680',
+            zipCode: '11680',
+            city: 'Algodonales'
+          }
+         */
+        const user = conv.user;
+        /*
+        console.log('Previous event', conv.data.event);
+        conv.followup(conv.data.event, {
+            coordinates: location.coordinates,
+            ...conv.data.originalParams
+        });
 
-     */
-    const query = conv.data.filter;
-    query.coordinates = location.coordinates;
-    const station = await seviciService.findStation(query);
-    const distance = geolib.getDistance(location.coordinates, station.position);
-    const direction = getDirection(location.coordinates, station.position)
+         */
+        const query = conv.data.filter;
+        query.coordinates = location.coordinates;
+        const station = await seviciService.findStation(query);
+        const distance = geolib.getDistance(location.coordinates, station.position);
+        const direction = getDirection(location.coordinates, station.position)
 
-    const humanizedName = station.name.replace(/\d+_/i, '');
+        const humanizedName = station.name.replace(/\d+_/i, '');
 
-    const textMessage = buildStationString(humanizedName, distance, direction, query);
-    conv.ask(textMessage);
-    conv.ask(new BasicCard({
-        text: `Distance: **${distance} meters**  \n
-            Available bikes: **${station.available_bikes}**  \n
-            Available stands: **${station.available_bike_stands}**  \n
-            Total stands: **${station.bike_stands}**  \n
-            Address: **${station.address}**  \n
-            Status: **${station.status}**  \n
-            Query: **${conv.data.originalParams.criteria.join(' ')}**
-            `,
-        // subtitle: 'This is a subtitle',
-        title: humanizedName,
-        buttons: [
-            new Button({
-                title: 'View on map',
-                url: buildUrl('https://www.google.com/maps/dir/', {
-                    queryParams: {
-                        api: 1,
-                        destination: `${station.position.lat},${station.position.lng}`
-                    }
+        const textMessage = buildStationString(humanizedName, distance, direction, query);
+        conv.ask(textMessage);
+        conv.ask(new BasicCard({
+            text: `Distance: **${distance} meters**  \n
+                Available bikes: **${station.available_bikes}**  \n
+                Available stands: **${station.available_bike_stands}**  \n
+                Total stands: **${station.bike_stands}**  \n
+                Address: **${station.address}**  \n
+                Status: **${station.status}**  \n
+                Query: **${conv.data.originalParams.criteria.join(' ')}**
+                `,
+            // subtitle: 'This is a subtitle',
+            title: humanizedName,
+            buttons: [
+                new Button({
+                    title: 'View on map',
+                    url: buildUrl('https://www.google.com/maps/dir/', {
+                        queryParams: {
+                            api: 1,
+                            destination: `${station.position.lat},${station.position.lng}`
+                        }
+                    })
+                }),
+                new Button({
+                    title: 'Navigate to',
+                    url: buildUrl('https://www.google.com/maps/dir/', {
+                        queryParams: {
+                            api: 1,
+                            destination: `${station.position.lat},${station.position.lng}`,
+                            dir_action: 'navigate'
+                        }
+                    })
                 })
-            }),
-            new Button({
-                title: 'Navigate to',
-                url: buildUrl('https://www.google.com/maps/dir/', {
+            ],
+            image: new Image({
+                url: buildUrl('https://maps.googleapis.com/maps/api/staticmap', {
                     queryParams: {
-                        api: 1,
-                        destination: `${station.position.lat},${station.position.lng}`,
-                        dir_action: 'navigate'
+                        markers: `${station.position.lat},${station.position.lng}`,
+                        size: `700x300`,
+                        key: process.env.STATICMAPAPIKEY
                     }
-                })
-            })
-        ],
-        image: new Image({
-            url: buildUrl('https://maps.googleapis.com/maps/api/staticmap', {
-                queryParams: {
-                    markers: `${station.position.lat},${station.position.lng}`,
-                    size: `700x300`,
-                    key: process.env.STATICMAPAPIKEY
-                }
+                }),
+                alt: 'dock location',
             }),
-            alt: 'dock location',
-        }),
-        display: 'CROPPED',
-    }));
+            display: 'CROPPED',
+        }));
     }
 }
