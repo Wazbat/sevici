4103d7a957e87877ac54af128f731788e7057da5
Warren <wazbat97@gmail.com>
2019-12-10 13:19:46 +0100

    :bug: Fixed part count case

--- intentFunctions/app.station.extra.partCount.js
+++ intentFunctions/app.station.extra.partCount.js
@@ -8,14 +8,14 @@ module.exports = async (agent) => {
     let responseString = '';
     switch (agent.parameters.stationPart) {
         case 'dock':
+        case 'free dock':
             responseString = `There are a total of ${updatedStation.bike_stands} stands, with ${updatedStation.available_bike_stands} unoccupied.`;
             break;
         case 'bicycle':
-            responseString = `There are ${updatedStation.available_bikes} bikes available, with room to park ${updatedStation.bike_stands - updatedStation.available_bike_stands} more.`;
+            responseString = `There are ${updatedStation.available_bikes} bikes available, with room to park ${updatedStation.available_bike_stands} more.`;
             break;
         default:
-            responseString = `There are ${updatedStation.available_bikes} bikes and ${updatedStation.bike_stands - updatedStation.available_bike_stands} free stands.`;
-            break;
+            responseString = `There are ${updatedStation.available_bikes} bikes and ${updatedStation.bike_stands - updatedStation.available_bikes} free stands.`;
     }
     if (updatedStation.status !== 'OPEN') responseString += ` Be aware, the station is currently ${updatedStation.status}`;
     conv.ask(responseString);
