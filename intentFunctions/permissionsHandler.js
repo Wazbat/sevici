d17d5cb98de0df9077d329b1bb586ff902169d06
Warren <wazbat97@gmail.com>
2019-12-22 17:23:53 +0100

    :recycle: Reworked permissions handler

--- intentFunctions/permissionsHandler.js
+++ intentFunctions/permissionsHandler.js
@@ -1,21 +1,14 @@
-const { stationSearch } = require("./permissionsHandlerFunctions/station-search");
-const { stationDistance } = require('./permissionsHandlerFunctions/station-distance');
 
+const handlerMap = new Map();
+handlerMap.set('station-search', require("./permissionsHandlerFunctions/station-search").stationSearch);
+handlerMap.set('station-distance', require('./permissionsHandlerFunctions/station-distance').stationDistance);
+handlerMap.set('station-route', require("./permissionsHandlerFunctions/station-route").routeSearch);
 module.exports = async (agent) => {
     const conv = agent.conv();
     if (conv) {
-        switch (conv.data.event) {
-            case 'station-search':
-                await stationSearch(conv);
-                agent.add(conv);
-                break;
-            case 'station-distance':
-                await stationDistance(conv);
-                agent.add(conv);
-                break;
-            default:
-                throw new Error(`Search handler with unhandled event: ${conv.data.event}`)
-        }
+        if (!handlerMap.has(conv.data.event)) throw new Error(`Search handler with unhandled event: ${conv.data.event}`);
+        await handlerMap.get(conv.data.event)(conv);
+        agent.add(conv);
     } else {
         throw new Error('No actions on google in permissions handler')
     }
