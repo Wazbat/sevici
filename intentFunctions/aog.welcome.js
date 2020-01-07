b6c9756f2ac1b2f6b96dd29835159b23c2250e49
Warren <wazbat97@gmail.com>
2020-01-07 18:12:08 +0100

    :speech_balloon: Shortened suggestion chip

--- intentFunctions/aog.welcome.js
+++ intentFunctions/aog.welcome.js
@@ -4,7 +4,7 @@ module.exports = async (agent) => {
     const conv = agent.conv();
     if (conv) {
         conv.ask('Welcome to Sevici Helper. What can I help you with today?');
-        conv.ask(new Suggestions(['Closest available bike', 'Closest available parking space']));
+        conv.ask(new Suggestions(['Closest available bike', 'Closest available dock']));
         agent.add(conv);
     } else {
         throw new Error('No actions on google in actions on google welcome handler')
