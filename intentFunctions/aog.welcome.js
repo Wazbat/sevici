8d15ae453a6b27784247946753f688e5b3a07abf
Warren <wazbat97@gmail.com>
2020-01-07 18:10:37 +0100

    :clown_face: Forgot to add conv to agent

--- intentFunctions/aog.welcome.js
+++ intentFunctions/aog.welcome.js
@@ -5,7 +5,8 @@ module.exports = async (agent) => {
     if (conv) {
         conv.ask('Welcome to Sevici Helper. What can I help you with today?');
         conv.ask(new Suggestions(['Closest available bike', 'Closest available parking space']));
+        agent.add(conv);
     } else {
-        throw new Error('No actions on google in permissions handler')
+        throw new Error('No actions on google in actions on google welcome handler')
     }
 };
