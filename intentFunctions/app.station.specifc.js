a8aeb05610a61c580b0bf852ed5c3c78997beae8
Warren <wazbat97@gmail.com>
2020-01-14 14:42:59 +0100

    :ambulance: Fixed specific station search. Still need to flesh it out

--- intentFunctions/app.station.specifc.js
+++ intentFunctions/app.station.specifc.js
@@ -6,19 +6,25 @@ module.exports = async (agent) => {
 
     // TODO Consider replacing conv with agent to allow for multiple platforms
     const conv = agent.conv();
-    let station;
-    // TODO Make number optional and also search by name using string similarity and a @sys.any to extract station name
-    if (conv.parameters.number) {
-        station = await seviciService.getStationByID(conv.parameters.number);
-        if (station) {
-            conv.ask(buildStationDetailsString(station, conv.user.locale));
-            conv.ask(generateStationCard(station, conv.user.locale));
-            conv.ask(new Suggestions(stringService.getString('distance from here', conv.user.locale)));
-            conv.contexts.set('station', 5, station);
+    if (conv) {
+        // TODO Make number optional and also search by name using string similarity and a @sys.any to extract station name
+        if (conv.parameters.number) {
+            const station = await seviciService.getStationByID(conv.parameters.number);
+            if (station) {
+                conv.ask(buildStationDetailsString(station, conv.user.locale));
+                conv.ask(generateStationCard(station, conv.user.locale));
+                conv.ask(new Suggestions(stringService.getString('distance from here', conv.user.locale)));
+                conv.contexts.set('station', 5, station);
+            } else {
+                conv.ask(stringService.getString('couldnt find any stations with id %{id}' , conv.user.locale).replace('%{id}', conv.parameters.number));
+            }
         } else {
-            conv.ask(stringService.getString('couldnt find any stations with id %{id}' , conv.user.locale).replace('%{id}', conv.parameters.number));
+            conv.ask('Specific station search but no number supplied')
         }
+
+        agent.add(conv)
+    } else {
+        agent.add('Not actions on google')
     }
 
-    agent.add(conv)
 };
