f370d866f54b479537a8c1b72a6ba85463152d6f
Warren <wazbat97@gmail.com>
2020-02-20 14:18:20 +0100

    :zap: Added google cloud profile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
     require('@google-cloud/debug-agent').start();
     require('@google-cloud/trace-agent').start();
+    require('@google-cloud/profiler').start();
 }
 const { WebhookClient} = require('dialogflow-fulfillment');
 const express = require('express');
